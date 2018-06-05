5bccd757efbf59c05edcc68a27bd7dadd4a8eaa7
Nic McElroy <nicamcelroy@gmail.com>
2018-06-05 22:32:07 +0100

    update tests

--- controls/example.rb
+++ controls/example.rb
@@ -1,19 +1,14 @@
 # encoding: utf-8
-# copyright: 2018, The Authors
-
-title 'sample section'
-
-# you can also use plain tests
-describe file('/tmp') do
-  it { should be_directory }
-end
+# copyright: 2018, Nic
 
 # you add controls here
-control 'tmp-1.0' do                        # A unique ID for this control
-  impact 0.7                                # The criticality, if this control fails.
-  title 'Create /tmp directory'             # A human-readable title
-  desc 'An optional description...'
-  describe file('/tmp') do                  # The actual test
-    it { should be_directory }
+control 'demorole control' do                        # A unique ID for this control
+  impact 1                                # The criticality, if this control fails.
+  title 'Create a file with specific contents'             # A human-readable title
+  desc 'C:\\demorolefile.txt shoul have the contents we desire'
+  describe file('C:\\demorolefile.txt') do                  # The actual test
+    it { should exist }
+    it { should be_file }
+    its('content') { should match(%r{This is the demorole file version 1}) }
   end
 end
